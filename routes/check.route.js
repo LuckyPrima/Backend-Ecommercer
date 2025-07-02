0e4efee56b947a940cd8d21252687686d6b6c850
luckyprima2002@gmail.com <luckyprima@Lucky-Prima.localdomain>
2025-07-02 23:23:37 +0700

    update new check

--- routes/check.route.js
+++ routes/check.route.js
@@ -3,7 +3,7 @@ import express from "express";
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.statusCode(200).json({ message: "Hello from check route" });
+  res.status(200).json({ message: "Hello from check route" });
 });
 
 export default router;
